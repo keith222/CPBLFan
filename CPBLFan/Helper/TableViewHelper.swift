8c47752f84f3f406df74ca3c7a9630efec5f1f82
Yang Tun-Kai <ks97121@gmail.com>
2016-12-26 23:50:57 +0800

    complete infinite table

--- CPBLFan/Helper/TableViewHelper.swift
+++ CPBLFan/Helper/TableViewHelper.swift
@@ -18,8 +18,16 @@ class TableViewHelper: NSObject {
     fileprivate let tableView: UITableView
     fileprivate let templateCell: UITableViewCell
     fileprivate let dataSource: DataSource
+    var savedData: [AnyObject] = [] {
+        didSet{
+            //this for refresh(load more)data
+            dataSource.data = savedData
+            dataSource.flag = true
+            tableView.reloadData()
+        }
+    }
     
-    init(tableView: UITableView, nibName: String, source: [AnyObject], selectAction: ((Int)->())? = nil) {
+    init(tableView: UITableView, nibName: String, source: [AnyObject], selectAction: ((Int)->())? = nil, refreshAction: ((Int)->())? = nil) {
         self.tableView = tableView
         let nib = UINib(nibName: nibName, bundle: nil)
         templateCell = nib.instantiate(withOwner: nil, options: nil)[0] as! UITableViewCell
@@ -29,14 +37,15 @@ class TableViewHelper: NSObject {
         
         super.init()
         
+        //set datasource variables
         dataSource.data = source
         dataSource.selectAction = selectAction!
+        dataSource.refreshAction = refreshAction!
+        dataSource.flag = true
+        
         self.tableView.dataSource = dataSource
         self.tableView.delegate = dataSource
         self.tableView.reloadData()
-        
-        
-        
     }
 }
 
@@ -45,17 +54,19 @@ class DataSource: NSObject, UITableViewDataSource, UITableViewDelegate {
     fileprivate let templateCell: UITableViewCell
     var data: [AnyObject]
     var selectAction: ((Int)->())?
+    var refreshAction: ((Int)->())?
+    var flag: Bool = true
     
-    init(data: [AnyObject], templateCell: UITableViewCell, selectAction: ((Int)->())? = nil) {
+    init(data: [AnyObject], templateCell: UITableViewCell, selectAction: ((Int)->())? = nil, refreshAction: ((Int)->())? = nil) {
         self.data = data
         self.templateCell = templateCell
         self.selectAction = selectAction
+        self.refreshAction = refreshAction
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return data.count
     }
-    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: templateCell.reuseIdentifier!)!
@@ -67,7 +78,6 @@ class DataSource: NSObject, UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        
         switch UIDevice.current.userInterfaceIdiom {
         case .pad:
             return UIScreen.main.bounds.size.height / 3
@@ -81,6 +91,21 @@ class DataSource: NSObject, UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         self.selectAction!(indexPath.row)
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    var page = 1
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        //if tableview can refresh(load more) data
+        if let refreshAction = self.refreshAction{
+            let offset = scrollView.contentOffset.y
+            let contentHeight = scrollView.contentSize.height
+    
+            if (contentHeight - scrollView.frame.size.height) - offset < 44 && flag{
+                page += 1
+                refreshAction(page)
+                flag = false
+            }
+        }
     }
     
 }
--- CPBLFan/View/ViewController/NewsViewController.swift
+++ CPBLFan/View/ViewController/NewsViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Kingfisher
 import PKHUD
+import DynamicColor
 
 class NewsViewController: UIViewController {
     
@@ -31,10 +32,25 @@ class NewsViewController: UIViewController {
         //set navigation bar title
         self.navigationItem.title = "CPBL Fans"
         
+        //set tableview layout
+        self.newsTableView.separatorStyle = .none
+        self.newsTableView.estimatedRowHeight = 200
+        self.newsTableView.rowHeight = UITableViewAutomaticDimension
+        
+        //set activity indicator in foot view
+        let footerView = UIView(frame: CGRect(x: 0, y: 5, width: self.view.bounds.size.width, height: 50))
+        let activity: UIActivityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+        activity.frame = CGRect(x: (self.view.bounds.size.width - 44) / 2, y: 5, width: 44, height: 44)
+        activity.transform = CGAffineTransform(scaleX: 2, y: 2)
+        footerView.addSubview(activity)
+        footerView.isHidden = true
+        self.newsTableView.tableFooterView = footerView
+        activity.startAnimating()
+        
         //load and show news from cpbl website
         self.newsViewModel.fetchNews(from: page, handler: { [unowned self] data in
             
-            let source: [NewsViewModel] = data.map{ value -> NewsViewModel in
+            var source: [NewsViewModel] = data.map{ value -> NewsViewModel in
                 return NewsViewModel(data: value)
             }
             
@@ -45,22 +61,29 @@ class NewsViewController: UIViewController {
                 source: source as [AnyObject],
                 selectAction:{ [unowned self] num in
                     //closure for tableview cell tapping
-                    
                     let destion: NewsContentViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "NewsContentViewController") as! NewsContentViewController
                     destion.newsUrl = data[num].newsUrl
                     destion.newsTitle = data[num].title
                     destion.newsDate = data[num].date
                     destion.newsImageUrl = data[num].imageUrl
                     self.navigationController?.pushViewController(destion, animated: true)
+                },
+                refreshAction:{ page in
+//                  closure for refresh(load more)data
+                    self.newsViewModel.fetchNews(from: (page - 1), handler: { [unowned self] data in
+                        let moreSource: [NewsViewModel] = data.map{ value -> NewsViewModel in
+                            return NewsViewModel(data: value)
+                        }
+                        source.append(contentsOf: moreSource)
+                        self.tableViewHelper?.savedData = source
+                    })
                 }
             )
             
+            footerView.isHidden = false
+            
             HUD.hide(animated: true)
         })
-        
-        self.newsTableView.separatorStyle = .none
-        self.newsTableView.estimatedRowHeight = 200
-        self.newsTableView.rowHeight = UITableViewAutomaticDimension
     }
     
     override func viewWillLayoutSubviews() {
