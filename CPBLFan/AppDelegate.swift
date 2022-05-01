d6bf0bd4142f43fd5046152fc5d74fad88e39221
Yang Tun-Kai <ks97121@gmail.com>
2022-05-01 16:29:59 +0800

    Add quick action feature

--- CPBLFan/AppDelegate.swift
+++ CPBLFan/AppDelegate.swift
@@ -14,13 +14,13 @@ import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-                
+        
         // Navigation bar style
         UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
         UINavigationBar.appearance().isTranslucent = true
@@ -32,7 +32,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // Tab bar style
         UITabBar.appearance().tintColor = .darkBlue
-
+        
         
         // Use Firebase library to configure APIs
         FirebaseApp.configure()
@@ -48,8 +48,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             completionHandler: {_, _ in })
         
         application.registerForRemoteNotifications()
-                
-        return true
+        
+        var isLaunchedFromQuickAction = false
+        if let shortcutItem = launchOptions?[UIApplication.LaunchOptionsKey.shortcutItem] as? UIApplicationShortcutItem {
+            isLaunchedFromQuickAction = true
+            let _ = handleShortoutAction(shortcutItem)
+        }
+        
+        return !isLaunchedFromQuickAction
+    }
+    
+    func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        completionHandler(handleShortoutAction(shortcutItem))
     }
     
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
@@ -83,29 +93,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         tabController.selectedIndex = 1
         self.window?.rootViewController = tabController
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
+    
     func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
         if let videoPlayer = self.window?.rootViewController?.presentedViewController as? VideoPlayerViewController {
             return (videoPlayer.videoPlayerViewModel?.isPresented ?? true) ? .all : .portrait
@@ -117,6 +127,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    private func handleShortoutAction(_ shortcutItem: UIApplicationShortcutItem) -> Bool {
+        var tabIndex = 0
+        
+        switch shortcutItem.type {
+        case "Schedule": tabIndex = 1
+        case "Rank": tabIndex = 2
+        case "Statistics": tabIndex = 3
+        case "Videos": tabIndex = 4
+        default: break;
+        }
+        
+        let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let tabController: UITabBarController = storyboard.instantiateInitialViewController() as! UITabBarController
+        tabController.selectedIndex = tabIndex
+        self.window?.rootViewController = tabController
+        
+        return true
     }
 }
 
@@ -139,7 +168,7 @@ extension AppDelegate : UNUserNotificationCenterDelegate {
         if let messageID = userInfo["gcm.message_id"] {
             print("Message ID: \(messageID)")
         }
-
+        
         if let aps = userInfo[AnyHashable("aps")] as? NSDictionary, let alert = aps["alert"] as? NSDictionary, let body = alert["body"] as? String, let url = getUrl(from: body) {
             UIApplication.shared.open(url)
             
@@ -151,11 +180,11 @@ extension AppDelegate : UNUserNotificationCenterDelegate {
     private func getUrl(from text: String) -> URL? {
         let types: NSTextCheckingResult.CheckingType = .link
         let detector = try? NSDataDetector(types: types.rawValue)
-
+        
         guard let detect = detector else { return nil }
-
+        
         let matches = detect.matches(in: text, options: .reportCompletion, range: NSMakeRange(0, text.count))
-
+        
         return matches.first?.url
     }
 }
