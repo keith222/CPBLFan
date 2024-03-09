92271782927e3a90de913c375f23d2b2f9a2e2a4
Yang Tun-Kai <ks97121@gmail.com>
2024-03-09 22:42:59 +0800

    Update models

--- CPBLFan/Model/Game.swift
+++ CPBLFan/Model/Game.swift
@@ -15,8 +15,12 @@ struct Game: Codable {
     let guest: String?
     let home: String?
     let place: String?
-    let g_score: String?
-    let h_score: String?
+    let gScore: String?
+    let hScore: String?
     let stream: String?
     let time: String?
+    
+    private enum CodingKeys: String, CodingKey {
+        case game, date, guest,home, place, gScore = "g_score", hScore = "h_score", stream, time
+    }
 }
--- CPBLFan/Model/Rank.swift
+++ CPBLFan/Model/Rank.swift
@@ -8,12 +8,24 @@
 
 import Foundation
 
-struct Rank {
+struct Season: Codable {
+    let first: [Rank]
+    let second: [Rank]
+    let full: [Rank]
+}
+
+struct Rank: Codable {
     
-    let team: String?
-    let win: String?
-    let lose: String?
-    let tie: String?
-    let percentage: String?
-    let gamebehind: String?
+    let team: String
+    let rank: Int
+    let win: Int
+    let lose: Int
+    let tie: Int
+    let displayRank: Int
+    let winningRate: Double
+    let gameBehind: Double
+    
+    private enum CodingKeys: String, CodingKey {
+        case team, rank, win, lose, tie, displayRank = "display_rank", winningRate = "winning_rate", gameBehind = "game_behind"
+    }
 }
